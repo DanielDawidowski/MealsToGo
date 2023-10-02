fdd7b4bae400ce3844cb716a7a08f71dac413c89
daniel <dvds1987@gmail.com>
2023-10-02 16:34:20 +0200

    Init Expo

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+
+const isAndroid = Platform.OS === "android";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={{ flex: 1, marginTop: isAndroid ? 50 : 0 }}>
+        <View style={{ padding: 16, backgroundColor: "green" }}>
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "red" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
